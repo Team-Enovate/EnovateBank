31c3f77f6333496b0f9dd8a8ce1bb719d3e4049d
Ayoolavictor <ayoolavictor415@gmail.com>
2023-03-28 22:54:36 +0100

    Added comments on the smart contracts

--- smartcontract/contracts/EnovateBank.sol
+++ smartcontract/contracts/EnovateBank.sol
@@ -7,17 +7,22 @@ import "./EnovateToken.sol";
 contract EnovateBank {
     address public owner;
     EnovateToken private enovateToken;
-
+    //mapping that will track the time the user deposits
     mapping(address => uint) public depositStart;
+    //mapping that tracks the Total interest gotten by the user
     mapping(address => uint) public TotalInterest;
+    //mapping that tracks the Ether balance of each user
     mapping(address => uint) public etherBalanceOf;
+    //mapping that tracks the Collateral deposited by the borrower in ether
     mapping(address => uint) public collateralEther;
+    //mapping that tracks the total deposit made by the user
     mapping(address => uint) public totalDeposit;
+    //mapping that tracks the total token minted by the user 
     mapping(address => uint) public totalTokensMinted;
-
+    
     mapping(address => bool) public isDeposited;
     mapping(address => bool) public isBorrowed;
-
+    // contract events 
     event Deposit(address indexed user, uint etherAmount, uint timeStart);
     event Withdraw(address indexed user, uint etherAmount);
     event Borrow(address indexed user, uint collateralEtherAmount, uint borrowedTokenAmount);
@@ -37,7 +42,7 @@ contract EnovateBank {
         enovateToken = _enovateToken;
     }
 
-
+   // This function allows the borrower to deposit ETH for savings
     function depositETH() payable public {
         require(msg.value > 0, 'Error, invalid amount');
         uint256 depositAmount = msg.value;
@@ -48,16 +53,17 @@ contract EnovateBank {
         depositors.push(msg.sender);
         emit Deposit(msg.sender, depositAmount, block.timestamp);
     }
-
+   // This function calculate the interest that will be gotten by the user at the time of savings withdrawal
+   //It also sends the interest gotten when the withdraw function is triggered
     function CalculateInterest(address userAdd) internal {
-        uint256 INTEREST_RATE = 1; // 10% annual interest rate
+        uint256 INTEREST_RATE = 1; // 1% interest of intial deposit per second
         uint256 currentDeposit = etherBalanceOf[userAdd];
         
         require (etherBalanceOf[userAdd] != 0, "no valid deposit yet");
 
         uint256 periodOfDeposite = block.timestamp - depositStart[userAdd];
-       // uint256 interest = currentDeposit * INTEREST_RATE * periodOfDeposite / (365 days * DECIMALS);
-        uint256 interest = currentDeposit * INTEREST_RATE * periodOfDeposite; // daliy 
+
+        uint256 interest = currentDeposit * INTEREST_RATE * periodOfDeposite; 
         enovateToken.transfer(msg.sender, interest);
         
 
@@ -70,7 +76,7 @@ contract EnovateBank {
         }
     }
 
-
+  // This function allows the user to withdraw ETH deposited and interest will also be sent with the ETH deposited
     function withdrawETH(uint256 _amount) public {
         require(isDeposited[msg.sender] == true, 'Error: no previous deposit');
         require(etherBalanceOf[msg.sender] >= _amount, 'Error: invalide amount');
@@ -85,7 +91,7 @@ contract EnovateBank {
 
         emit Withdraw(msg.sender, _amount);
     }
-
+   // User can borrow ENOVATE tokens here by depositing ether, 1 Ether ==100 ENOVATE tokens
     function borrow() public payable {
         require(msg.sender != address(0) && msg.value > 0, 'Error: invalid address, amount, or value sent');
         require(isBorrowed[msg.sender] == false, 'Error: loan already active');
@@ -98,15 +104,13 @@ contract EnovateBank {
 
         emit Borrow(msg.sender,msg.value, tokensToMint);
     }
-    
+    // This functions handles when the user want to pay back the loan, 90% of the Collateral is returned back has the 
+    //bank service charge, and 5% of the Token that was borrowed is given to the user has a gift
     function payoffToken() public{
         require(isBorrowed[msg.sender], 'Error: no active loan');
-         uint tokensToBurn = totalTokensMinted[msg.sender] * 95 / 100; 
+         uint tokensToBurn = totalTokensMinted[msg.sender] * 95 / 100; //Transfers 95% back to the bank contract
          require(enovateToken.transferFrom(msg.sender,address(this),tokensToBurn));
         uint transferAmount= collateralEther[msg.sender] * 90/ 100; //takes a service fee of 10%
-        
-        
-       
         totalTokensMinted[msg.sender] -= tokensToBurn;
         payable(msg.sender).transfer(transferAmount);
         collateralEther[msg.sender] = 0;
--- smartcontract/contracts/EnovateToken.sol
+++ smartcontract/contracts/EnovateToken.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+//Token contract 
 
 contract EnovateToken {
     string public name;
@@ -12,14 +13,14 @@ contract EnovateToken {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event Burn(address indexed burner, uint256 value);
-
+   //Token is minted as contract is deployed
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply;
         balanceOf[msg.sender] = _totalSupply;
     }
-
+    // This function enables the transfer of ENOVATE tokens from this contract to the bank
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
         require(_to != address(0), "Invalid recipient address");
@@ -29,13 +30,13 @@ contract EnovateToken {
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
-
+    // This function makes it possible for a user to approve the bank to spend their Enovate tokens 
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
-
+   // This function allows transfer of tokens from the user acc to the bank 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[_from] >= _value, "Insufficient balance");
         require(allowance[_from][msg.sender] >= _value, "Insufficient allowance");
@@ -47,7 +48,7 @@ contract EnovateToken {
         emit Transfer(_from, _to, _value);
         return true;
     }
-
+  // Used by token deployer to burn tokens 
     function burn(uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
 
